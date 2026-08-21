1f5305a0500e41245356c0fcd3740a1a6c282db5
unknown <adel.ayman2006@gmail.com>
2026-08-21 22:40:24 +0300

    Remove dist

--- dist/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { signin, signup } from "../controllers/authController.js";
-import validatSignup from "../middlewares/validateSignup.js";
-const router = Router();
-router.post("/signup", validatSignup, signup);
-router.post("/signin", signin);
-export default router; // we can import this router without{} and with anyname
--- dist/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protect, restrictTo } from "../middlewares/auth.js";
-import { createBooking, cancelBooking, getMyBookings, getSessionBookings, } from "../controllers/bookingController.js";
-const router = Router();
-router.post("/", protect, restrictTo("Member"), createBooking);
-router.patch("/:id/cancel", protect, restrictTo("Member"), cancelBooking);
-router.get("/my-bookings", protect, restrictTo("Member"), getMyBookings);
-router.get("/session/:sessionId", protect, restrictTo("Trainer"), getSessionBookings);
-export default router;
